92c681c223b7be6812aa8c402edb61885e194394
Fun Coding <155822962+FunCodingYT@users.noreply.github.com>
2025-12-01 12:09:56 +0400

    Add files via upload

---
+++ Episode 09/Introduction/intro.js
@@ -0,0 +1,53 @@
+// ➡️ Introduction to DOM
+
+// console.log(document.title);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+
+
+// ➡️ Accessing Elements
+
+// ◉ getElementsByTagName
+// let heading = document.getElementsByTagName("h1");
+// console.log(heading);
+
+// ◉ getElementsByClassName
+// let para = document.getElementsByClassName("para");
+// console.log(para);
+
+// ◉ getElementById
+// let myList = document.getElementById("list");
+// console.log(myList);
+
+// ◉ querySelector
+// let select = document.querySelector("#list");
+// console.log(select);
+
+// ◉ querySelectorAll
+// let selectAll = document.querySelectorAll("li");
+// console.log(selectAll);
+
+
+// ➡️ Changing Content
+
+// let heading = document.querySelector("h1");
+// console.log(heading.innerText);
+
+// heading.textContent = "Text Content nai muje change krdia!";
+// heading.innerText = "Inner Text nai bhi muje change krdia!";
+// heading.innerHTML = "<i>Changed!</i>";
+
+
+// ➡️ Changing Styles
+
+// let heading = document.querySelector("h1");
+// heading.classList.toggle("change");
+// console.log(heading);
+
+// heading.style.backgroundColor = "teal";
+// heading.style.fontSize = "3rem";
+// heading.style.borderColor = "black";
+// heading.style.borderWidth = "2px";
+// heading.style.borderStyle = "dotted";
+// console.log(heading.style);
