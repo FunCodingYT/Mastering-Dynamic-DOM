089d93143307c77199d175f57083216aef31c0ec
Fun Coding <155822962+FunCodingYT@users.noreply.github.com>
2025-12-01 12:06:05 +0400

    Add files via upload

---
+++ Episode 09/Event Handling/event.js
@@ -0,0 +1,12 @@
+// ➡️ Events And Event Handling
+
+// Events are actions that happens on a webpage.
+
+// const box = document.querySelector("#box");
+
+// function changeColor() {
+//     box.style.backgroundColor = "orange";
+// }
+
+// box.addEventListener("mouseover", changeColor);
+// box.removeEventListener("mouseover", changeColor);
