ddc42e9623b1d259a017171a8c0f0c381e59aa25
Fun Coding <155822962+FunCodingYT@users.noreply.github.com>
2025-12-01 12:08:08 +0400

    Add files via upload

---
+++ Episode 09/Event Object/object.js
@@ -0,0 +1,50 @@
+// ➡️ Event Object
+
+// 1) click Event
+// const btn =  document.querySelectorAll("button");
+
+// btn.forEach((button)=>{
+//     button.addEventListener("click", (event)=>{
+//         console.log(event.target.id);
+        
+//     })
+// })
+
+
+// 2) mousemove Event
+// const box = document.querySelector("#box");
+// const info = document.querySelector("#info");
+
+// window.addEventListener("mousemove", (event)=>{
+//    info.textContent = `Mouse Position: X-${event.clientX} | Y-${event.clientY}`;
+        
+// })
+
+
+// 3) input Event
+// const input =  document.querySelector("#input");
+// const message =  document.querySelector("#message");
+
+// input.addEventListener("input", (event)=>{
+//     message.textContent = event.target.value;
+    
+// })
+
+
+// 4) keyup Event
+// const letter =  document.querySelector("#letter");
+
+// document.addEventListener("keyup", (event)=>{
+//     letter.textContent = event.key;
+    
+// })
+
+
+// 5) submit Event
+// const form = document.querySelector("#form");
+
+// form.addEventListener("submit", (event)=>{
+//     event.preventDefault();
+//     console.log(event);
+    
+// })
